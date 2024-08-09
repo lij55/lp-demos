f7f05d6687a4a227b057dc9fec3b3d4ce6f71cf5
lij55 <liyang.jasper@gmail.com>
2024-08-09 18:33:46 +0800

    arguments work, not optimized

--- nurse.py
+++ nurse.py
@@ -1,9 +1,5 @@
-import time
-
 import streamlit as st
 from schedule import solve_schedule
-
-
 import pandas as pd
 
 
@@ -15,20 +11,21 @@ st.set_page_config(
 args = {}
 
 st.header('护士APN排班示例')
-num_nurses = st.number_input('护士人数', format="%d", value=20, step=1, min_value=1, max_value=40)
+st.info('周排班，每个护士5个班次， 每天最多一个班次，夜班尽可能平均')
+num_nurses = st.number_input('护士人数', format="%d", value=15, step=1, min_value=1, max_value=30)
 
 group_shift = st.container(border=True)
 col1, col2, col3 = group_shift.columns(3)
 shift = col1.selectbox('班次', [3], )
 
 
-col2.number_input(f'N班人数', format="%d", value=4, step=1, min_value=1, max_value=10)
-col2.number_input(f'A班人数', format="%d", value=4, step=1, min_value=1, max_value=10)
-col2.number_input(f'P班人数', format="%d", value=4, step=1, min_value=1, max_value=10)
+args["num_workday_n"] = col2.number_input(f'N班人数', format="%d", value=2, step=1, min_value=1, max_value=10)
+args["num_workday_a"] = col2.number_input(f'A班最少人数', format="%d", value=4, step=1, min_value=1, max_value=10)
+args["num_workday_p"] = col2.number_input(f'P班最少人数', format="%d", value=3, step=1, min_value=1, max_value=10)
 
-col3.number_input(f'周末N班人数', format="%d", value=2, step=1, min_value=1, max_value=10)
-col3.number_input(f'周末A班人数', format="%d", value=2, step=1, min_value=1, max_value=10)
-col3.number_input(f'周末P班人数', format="%d", value=2, step=1, min_value=1, max_value=10)
+args["num_weekend_n"] = col3.number_input(f'周末N班人数', format="%d", value=2, step=1, min_value=1, max_value=10)
+args["num_weekend_a"] = col3.number_input(f'周末A班人数', format="%d", value=2, step=1, min_value=1, max_value=10)
+args["num_weekend_p"] = col3.number_input(f'周末P班人数', format="%d", value=2, step=1, min_value=1, max_value=10)
 
 # st.data_editor('Edit data', data)
 
@@ -36,25 +33,36 @@ group_rest = st.container(border=True)
 c_rest = group_rest.checkbox('休息班次')
 
 if c_rest:
-    group_rest.number_input(f'N班后休息班数', format="%d", value=4, step=1, min_value=1, max_value=5)
-    group_rest.number_input(f'A班后休息班数', format="%d", value=1, step=1, min_value=1, max_value=5)
-    group_rest.number_input(f'P班后休息班数', format="%d", value=1, step=1, min_value=1, max_value=5)
+    args["extra_rest_constrain"] = True
+    args["extra_rest_n"] = group_rest.number_input(f'N班后休息班数', format="%d", value=4, step=1, min_value=1, max_value=5)
+    args["extra_rest_a"] = group_rest.number_input(f'A班后休息班数', format="%d", value=1, step=1, min_value=1, max_value=5)
+    args["extra_rest_p"] = group_rest.number_input(f'P班后休息班数', format="%d", value=1, step=1, min_value=1, max_value=5)
+
 
 def int_to_name(v):
     if v == 0:
         return ""
     if v == 1:
         return "N"
-    if v == 2:
+    if v == 10:
         return "A"
-    if v == 3:
+    if v == 100:
         return "P"
+    if v == 11:
+        return "N+A"
+    if v == 110:
+        return "A+P"
+    if v == 101:
+        return "N+P"
+    if v == 111:
+        return "N+A+P"
+    print(v)
 
-clicked = st.button('求解')
+
 result = solve_schedule(num_nurses, args)
 if result is not None:
     #st.write(result)
-    df = pd.DataFrame(result, columns=['一','二','三','四','五','六','日',]).applymap(int_to_name)
+    df = pd.DataFrame(result, columns=['一','二','三','四','五','六','日',]).map(int_to_name)
     st.dataframe(df,use_container_width=True)
     df1 = pd.DataFrame()
 
@@ -62,5 +70,5 @@ if result is not None:
         df1[c] = df[c].value_counts()
     st.write(df1)
 else:
-    st.write('无法满足条件')
-
+    st.warning('无法满足条件')
+st.button('重新计算')
--- schedule.py
+++ schedule.py
@@ -16,6 +16,7 @@ total_shifts = num_days * NUM_SHIFTS
 
 
 def solve_schedule(num_nurses, args):
+    print(args)
     model = cp_model.CpModel()
     shifts = {}
 
@@ -27,44 +28,58 @@ def solve_schedule(num_nurses, args):
                 shifts[(n, s_number)] = model.NewBoolVar(f'shift_n{n}s{s_number}')
                 model.AddHint(shifts[(n, s_number)], np.random.randint(0, 2))  # 设置随机初始值, 或者设置预设值
 
-    # A2,P2,N1 work_days; A1,P1,N1 work_days;
-    # for ts in range(0, total_shifts, NUM_SHIFTS):
-    #     model.Add(sum(shifts[(n, ts)] for n in range(num_nurses)) == 1) #N
-    #     model.Add(sum(shifts[(n, ts + 1)] for n in range(num_nurses)) >= 2) #A
-    #     model.Add(sum(shifts[(n, ts + 2)] for n in range(num_nurses)) >= 2) #P
+
+    num_workday_n = args.get("num_workday_n", 0)
+    num_workday_a = args.get("num_workday_a", 0)
+    num_workday_p = args.get("num_workday_p", 0)
+
+    num_weekend_n = args.get("num_weekend_n", 0)
+    num_weekend_a = args.get("num_weekend_a", 0)
+    num_weekend_p = args.get("num_weekend_p", 0)
 
     for w in range(num_weeks):
 
         # Sunday
-        model.Add(sum(shifts[(n, (w * 7 + 6) * NUM_SHIFTS)] for n in range(num_nurses)) == 2)  # N
-        model.Add(sum(shifts[(n, (w * 7 + 6) * NUM_SHIFTS + 1)] for n in range(num_nurses)) == 2)  # A
-        model.Add(sum(shifts[(n, (w * 7 + 6) * NUM_SHIFTS + 2)] for n in range(num_nurses)) == 2)  # P
+        model.Add(sum(shifts[(n, (w * 7 + 6) * NUM_SHIFTS)] for n in range(num_nurses)) == num_weekend_n)  # N
+        model.Add(sum(shifts[(n, (w * 7 + 6) * NUM_SHIFTS + 1)] for n in range(num_nurses)) == num_weekend_a)  # A
+        model.Add(sum(shifts[(n, (w * 7 + 6) * NUM_SHIFTS + 2)] for n in range(num_nurses)) == num_weekend_p)  # P
 
         # work day
         for d in range(5):
-            model.Add(sum(shifts[(n, (w * 7 + d) * NUM_SHIFTS)] for n in range(num_nurses)) == 2)  # N
-            model.Add(sum(shifts[(n, (w * 7 + d) * NUM_SHIFTS + 1)] for n in range(num_nurses)) >= 5)  # A
-            model.Add(sum(shifts[(n, (w * 7 + d) * NUM_SHIFTS + 2)] for n in range(num_nurses)) >= 4)  # P
+            model.Add(sum(shifts[(n, (w * 7 + d) * NUM_SHIFTS)] for n in range(num_nurses)) == num_workday_n)  # N
+            model.Add(sum(shifts[(n, (w * 7 + d) * NUM_SHIFTS + 1)] for n in range(num_nurses)) >= num_workday_a)  # A
+            model.Add(sum(shifts[(n, (w * 7 + d) * NUM_SHIFTS + 2)] for n in range(num_nurses)) >= num_workday_p)  # P
 
         # Saturday
-        model.Add(sum(shifts[(n, (w * 7 + 5) * NUM_SHIFTS)] for n in range(num_nurses)) == 2)  # N
-        model.Add(sum(shifts[(n, (w * 7 + 5) * NUM_SHIFTS + 1)] for n in range(num_nurses)) == 2)  # A
-        model.Add(sum(shifts[(n, (w * 7 + 5) * NUM_SHIFTS + 2)] for n in range(num_nurses)) == 2)  # P
+        model.Add(sum(shifts[(n, (w * 7 + 5) * NUM_SHIFTS)] for n in range(num_nurses)) == num_weekend_n)  # N
+        model.Add(sum(shifts[(n, (w * 7 + 5) * NUM_SHIFTS + 1)] for n in range(num_nurses)) == num_weekend_a)  # A
+        model.Add(sum(shifts[(n, (w * 7 + 5) * NUM_SHIFTS + 2)] for n in range(num_nurses)) == num_weekend_p)  # P
 
+    # 每天最多一个班次
+    for n in range(num_nurses):
+        for d in range(num_days):
+            model.Add(sum(shifts[(n,d * NUM_SHIFTS + i)] for i in range(NUM_SHIFTS)) <= 1) #5 shifts each week
 
-
+    # 每周5天
     for n in range(num_nurses):
         for w in range(num_weeks):
-            model.Add(sum(shifts[(n, w * 7 + ds)] for ds in range(7*NUM_SHIFTS)) == 5) #5days each week
+            model.Add(sum(shifts[(n, w * 7 * NUM_SHIFTS + ds)] for ds in range(7*NUM_SHIFTS)) <= 5) #5 shifts each week
 
-    # A班后至少休1个班次，P休1，N休3
-    for n in range(num_nurses):
-        for ts in range(0, total_shifts - NUM_SHIFTS, NUM_SHIFTS):  # N班
-            model.Add(sum([shifts[(n, ts + i)] for i in range(0, 4)]) <= 1)
-        for ts in range(1, total_shifts, NUM_SHIFTS):  # A班
-            model.Add(sum([shifts[(n, ts + i)] for i in range(0, 2)]) <= 1)
-        for ts in range(2, total_shifts - NUM_SHIFTS, NUM_SHIFTS):  # P班
-            model.Add(sum([shifts[(n, ts + i)] for i in range(0, 2)]) <= 1)
+    # A班后至少休几个班次
+
+    extra_rest_constrain = args.get("extra_rest_constrain", False)
+
+    if extra_rest_constrain:
+        extra_rest_n = args.get("extra_rest_n", 3)
+        extra_rest_a = args.get("extra_rest_a", 1)
+        extra_rest_p = args.get("extra_rest_p", 1)
+        for n in range(num_nurses):
+            for ts in range(0, total_shifts - NUM_SHIFTS, NUM_SHIFTS):  # N班
+                model.Add(sum([shifts[(n, ts + i)] for i in range(0, extra_rest_n + 1)]) <= 1)
+            for ts in range(1, total_shifts - NUM_SHIFTS, NUM_SHIFTS):  # A班
+                model.Add(sum([shifts[(n, ts + i)] for i in range(0, extra_rest_a + 1)]) <= 1)
+            for ts in range(2, total_shifts - NUM_SHIFTS, NUM_SHIFTS):  # P班
+                model.Add(sum([shifts[(n, ts + i)] for i in range(0, extra_rest_p + 1)]) <= 1)
 
     max_work_nights = model.NewIntVar(0, num_days, 'max_work_nights')
     min_work_nights = model.NewIntVar(0, num_days, 'min_work_nights')
@@ -73,14 +88,6 @@ def solve_schedule(num_nurses, args):
 
     model.AddMaxEquality(max_work_nights, work_nights)
     model.AddMinEquality(min_work_nights, work_nights)
-
-    work_days = [sum(shifts[(n, ts)] for ts in range(total_shifts)) for n in range(num_nurses)]
-
-    max_work_days = model.NewIntVar(0, num_days, 'max_work_days')
-    min_work_days = model.NewIntVar(0, num_days, 'min_work_days')
-
-    model.AddMaxEquality(max_work_days, work_days)
-    model.AddMinEquality(min_work_days, work_days)
 
     # model.Minimize((max_work_nights - min_work_nights)*10 + (max_work_days - min_work_days) * 10  )
     model.Minimize((max_work_nights - min_work_nights) )
@@ -91,19 +98,25 @@ def solve_schedule(num_nurses, args):
             #         model.Add(sum([shifts[(n, w * 7 + d)] for d in range(0,7)]) >= 4)
 
     solver = cp_model.CpSolver()
-    solver.parameters.num_search_workers = 8
+    #solver.parameters.num_search_workers = 8
     solver.parameters.cp_model_presolve = False
 
     status = solver.Solve(model)
-
+    r_shifts = {}
     if status == cp_model.OPTIMAL:
+        # for n in range(num_nurses):
+        #     for d in range(num_days):
+        #         for s in range(NUM_SHIFTS):
+        #             s_number = d * NUM_SHIFTS + s
+        #             r_shifts[(n, s_number)] =  solver.BooleanValue(shifts[(n, d * NUM_SHIFTS + s)])
+        # print(r_shifts)
         #schedule = [[[0 for _ in range(NUM_SHIFTS)] for _ in range(num_days)] for _ in range(num_nurses)]
         schedule = np.zeros((num_nurses, num_days))
         for d in range(num_days):
             for s in range(NUM_SHIFTS):
                 for n in range(num_nurses):
                     if solver.BooleanValue(shifts[(n, d * NUM_SHIFTS + s)]):
-                        schedule[n][d] = s + 1
+                        schedule[n][d] += 10**s
         return schedule
     else:
         return None
